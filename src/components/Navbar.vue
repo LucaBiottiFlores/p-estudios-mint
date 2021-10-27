a3e547a1ec9baca718090802da1bd5ffcce5a902
angela <angela.valdebenito.v@gmail.com>
2021-10-26 21:56:13 -0300

    avance navbar y router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-app-bar app dark>
+      <v-toolbar-title>Estudios Mint</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-home</v-icon>
+        logo
+      </v-btn>
+      <v-col class="mb-3" cols="12">
+        <v-row justify="end" class="mr-5">
+          <v-btn
+            color="white"
+            link
+            text
+            v-for="(link, i) in links"
+            :key="i"
+            :to="link.to"
+          >
+            {{ link.name }}
+          </v-btn>
+        </v-row>
+      </v-col>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data: () => ({
+    links: [
+      {
+        name: "Inicio",
+        to: "/",
+      },
+      {
+        name: "Agenda tu hora",
+        to: "/agendar-hora",
+      },
+      {
+        name: "Tienda",
+        to: "/tienda",
+      },
+      {
+        name: "Carrito de compras",
+        to: "/carrito-de-compras",
+      },
+    ],
+  }),
+};
+</script>
+
+<style scoped></style>
