b7e84474fd609cad7c726b8d42f4807b4c4eb1f9
LucaBiottiFlores <82482181+LucaBiottiFlores@users.noreply.github.com>
2021-11-03 22:05:21 -0300

    avances en las secciones del home, faltan detalles e integrar API Spotify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="d-flex flex-column">
     <video autoplay muted loop id="homeVideo">
-      <source src="../assets/video/videoHome.mp4" type="video/mp4" />
+      <source
+        src="https://firebasestorage.googleapis.com/v0/b/p-estudios-mint.appspot.com/o/videoHome.mp4?alt=media&token=dd8bd306-59e5-49bb-b3cc-b5e1daa6140d"
+        type="video/mp4"
+      />
     </video>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({})
+}
 </script>
 
 <style>
