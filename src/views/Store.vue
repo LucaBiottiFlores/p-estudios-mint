b499c0cd6db6fc87c5ac90f22a701b49bc05564f
angela <angela.valdebenito.v@gmail.com>
2021-11-13 17:52:21 -0300

    agregue modal para tienda al comprar

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -8,22 +8,22 @@
     </main>
     <v-container class="form_container" width="100vw">
       <label class="mt-5 ms-5 form_title">Busca tu producto:</label>
-      <v-text-field
-        class="ms-3 form_filter"
-        label=" Filtra por nombre, categoría, color o descripción"
+      <input
+        type="text"
+        class="ms-3 mt-3 form_filter"
+        placeholder=" Filtra por nombre, categoría, color o descripción"
         :value="$store.state.search"
         @input="$store.dispatch('setSearch', $event.target.value)"
-      >
-      </v-text-field>
+      />
     </v-container>
-    <ProductList />
-    <!-- <ProductList
-      v-if="$store.getters['products/searchedProducts'] > 0"
+    <!-- <ProductList /> -->
+    <ProductList
+      v-if="$store.getters['products/searchedProducts'].length > 0"
       :products="$store.getters['products/searchedProducts']"
     />
-    <div>
+    <div v-else>
       <div class="my-5">Producto no encontrado! D:</div>
-    </div> -->
+    </div>
   </v-app>
 </template>
 
@@ -31,6 +31,7 @@
 import ProductList from "../components/ProductList.vue";
 
 export default {
+  name: "products",
   components: {
     ProductList,
   },
@@ -71,5 +72,6 @@ export default {
 .form_filter {
   width: 400px;
   height: 40px;
+  border: solid 1px #95f5bd;
 }
 </style>
