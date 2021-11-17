1b7b3e6bc19e18c08a1aa9097fe37ca6bb583616
LucaBiottiFlores <82482181+LucaBiottiFlores@users.noreply.github.com>
2021-11-16 23:54:06 -0300

    login admin creado, falta arreglar detalles y visualizacion en calendario

--- src/store/modules/sesion.js
+++ src/store/modules/sesion.js
@@ -1,62 +1,24 @@
-import Firebase from "firebase";
-import Router from "../../router";
-
 export const ModuloSesion = {
   namespaced: true,
   state: {
-    user: null,
+    user: null
   },
 
   getters: {
-    isAdmin(state) {
-      return state.user.role === "admin";
-    },
-    isUser(state) {
-      return state.user.role === "user";
-    },
+    sendingUser(state) {
+      return state.user
+    }
   },
 
   mutations: {
-    SET_USER(state, newUser) {
-      state.user = newUser;
-    },
+    MUTATE_LOCAL_USER(state, userData) {
+      state.user = userData
+    }
   },
 
   actions: {
-    subscribeToAuthStateChange(context) {
-      Firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-          Firebase.firestore()
-            .collection("users")
-            .get()
-            .then((documents) => {
-              const newUser = {
-                email: user.email,
-                rol: "user",
-              };
-              documents.forEach((document) => {
-                const data = document.data();
-                if (data.email === user.email) {
-                  newUser.role = data.role;
-                  newUser.id = document.id;
-                }
-              });
-              context.commit("SET_USER", { ...newUser });
-              Router.push({ name: "Home" });
-            });
-        } else {
-          context.commit("SET_USER", null);
-        }
-      });
-    },
-    async signIn(_context, credentials) {
-      await Firebase.auth().signInWithEmailAndPassword(
-        credentials.email,
-        credentials.password
-      );
-    },
-    async signOut() {
-      await Firebase.auth().signOut();
-    },
-  },
-};
+    loadingUser({ commit }, userData) {
+      commit('MUTATE_LOCAL_USER', userData)
+    }
+  }
+}
