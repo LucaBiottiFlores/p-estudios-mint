7150a6a2367d5e1cfca72b09a98e699825637148
angela <angela.valdebenito.v@gmail.com>
2021-11-17 20:16:14 -0300

    arreglado error filtro y editar producto

--- src/views/EditProduct.vue
+++ src/views/EditProduct.vue
@@ -1,17 +1,111 @@
 <template>
   <v-container>
-    <EditProduct />
+    <v-card>
+      <v-container>
+        <h1>Editar producto: {{ $route.params.id }}</h1>
+        <v-form @submit.prevent="SaveChanges" ref="form">
+          <v-text-field
+            v-model="product.name"
+            type="text"
+            label="Nombre"
+          ></v-text-field>
+          <v-text-field
+            v-model.number="product.price"
+            type="number"
+            label="Precio"
+          ></v-text-field>
+          <v-text-field
+            v-model="product.category"
+            type="text"
+            label="Categoria"
+          ></v-text-field>
+          <v-text-field
+            v-model="product.color"
+            type="text"
+            label="Color"
+          ></v-text-field>
+          <v-text-field
+            v-model="product.description"
+            type="text"
+            label="Descripción"
+          ></v-text-field>
+          <v-text-field
+            v-model="product.src"
+            type="text"
+            label="Imagen"
+          ></v-text-field>
+          <v-btn type="submit" color="success">Guardar datos</v-btn>
+        </v-form>
+      </v-container>
+    </v-card>
   </v-container>
 </template>
-
 <script>
-import EditProduct from "../components/EditProduct.vue";
+import Firebase from "firebase";
 
 export default {
-  components: {
-    EditProduct,
+  beforeRouteEnter(to, from, next) {
+    console.log("PARAMS", to.params.id);
+    return Firebase.firestore()
+      .collection("products")
+      .doc(to.params.id)
+      .get()
+      .then((document) => {
+        console.log("DOCUMENT")
+        next((vm) => {
+          vm.product = { id: document.id, ...document.data() };
+        });
+      });
+  },
+  data: () => ({
+    product: {},
+  }),
+
+  methods: {
+    SaveChanges() {
+      console.log(this.product);
+      if (this.$refs.form.validate()) {
+        Firebase.firestore()
+          .collection("products")
+          .doc(this.product.id)
+          .update(this.product)
+          .then(() => {
+            this.$router.push("/administrar");
+          });
+      }
+    },
+    // validate() {
+    //   this.$refs.form.validate();
+    //   if (this.$refs.form.validate()) {
+    //     let newProduct = {
+    //       price: parseInt(this.price),
+    //       name: this.name,
+    //       category: this.category,
+    //       color: this.color,
+    //       src: this.src,
+    //       description: this.description,
+    //     };
+    //     this.$store.dispatch("updateProduct", newProduct).then(() => {
+    //       setTimeout(() => {
+    //         this.$router.replace({ name: "Administrar productos" });
+    //       }, 1000);
+    //     });
+    //   }
+    // },
+    // mounted() {
+    //   let foundProduct = this.sendingProducts.find(
+    //     (result) => result.productId === this.id
+    //   );
+    //   if (foundProduct !== undefined) {
+    //     this.price = parseInt(foundProduct.price);
+    //     this.name = foundProduct.name;
+    //     this.category = foundProduct.category;
+    //     this.color = foundProduct.color;
+    //     this.src = foundProduct.src;
+    //     this.description = foundProduct.description;
+    //   }
+    // },
   },
 };
 </script>
 
-<style scoped></style>
