edfc5335589a0428955fd06656509c55125798e1
LucaBiottiFlores <82482181+LucaBiottiFlores@users.noreply.github.com>
2021-11-18 16:13:30 -0300

    estilos

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,12 @@
         </v-btn>
 
         <div class="d-flex justify-center ml-9">
-          <v-btn v-if="$store.state.sesion.user" icon @click="handleLogout" color="white">
+          <v-btn
+            v-if="$store.state.sesion.user"
+            icon
+            @click="handleLogout"
+            color="white"
+          >
             <span class="ml-3 text-caption d-sm-inline" alt="Salir"
               >LogOut</span
             >
